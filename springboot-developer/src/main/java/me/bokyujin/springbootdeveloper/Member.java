83e2b7faca0133e1e690b3f3ebc6d12dac27f444
bokyujin <yjbok@beintech.co.kr>
2025-03-19 17:29:21 +0900

    chap 05

--- springboot-developer/src/main/java/me/bokyujin/springbootdeveloper/Member.java
+++ springboot-developer/src/main/java/me/bokyujin/springbootdeveloper/Member.java
@@ -6,18 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access= AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Getter
-@Entity
+@Entity //엔티티로 지정    -> @ENtity(name="member_list") 작성시 member_list라는 테이블과 매핑
+@NoArgsConstructor(access= AccessLevel.PROTECTED) //기본 생성자
+@AllArgsConstructor
 public class Member {
-    @Id
+    @Id //id 필드를 기본키로 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //기본키를 자동으로 1씩 증가
     @Column(name="id", updatable = false)
     private Long id;
 
-    @Column(name="name", updatable = false)
+    @Column(name="name", updatable = false) //name이라는 not null 컬럼과 매핑
     private String name;
+
+    public void changeName(String name){
+        this.name = name;
+    }
 
 
 }
