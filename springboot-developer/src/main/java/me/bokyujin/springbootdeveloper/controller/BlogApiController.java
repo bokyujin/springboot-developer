9a30b99674c31ed4f8cd8ea1d06e37dd1b9e8cb6
bokyujin <yjbok@beintech.co.kr>
2025-03-27 10:00:09 +0900

    chap 06

--- springboot-developer/src/main/java/me/bokyujin/springbootdeveloper/controller/BlogApiController.java
+++ springboot-developer/src/main/java/me/bokyujin/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,14 @@ package me.bokyujin.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.bokyujin.springbootdeveloper.domain.Article;
 import me.bokyujin.springbootdeveloper.dto.AddArticleRequest;
+import me.bokyujin.springbootdeveloper.dto.ArticleResponse;
+import me.bokyujin.springbootdeveloper.dto.UpdateArticleRequest;
 import me.bokyujin.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,11 +18,43 @@ public class BlogApiController {
 
     private final BlogService blogService;
 
-    @PostMapping("api/articles")
+    @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable("id") long id) {
+        Article article = blogService.findById(id);
+
+        return  ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable("id") long id) {
+        blogService.delete(id);
+
+        return  ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable("id") long id, @RequestBody UpdateArticleRequest request){
+        Article updatedArticle = blogService.update(id,request);
+
+        return ResponseEntity.ok().body(updatedArticle);
     }
 }
--- springboot-developer/src/main/java/me/bokyujin/springbootdeveloper/service/BlogService.java
+++ springboot-developer/src/main/java/me/bokyujin/springbootdeveloper/service/BlogService.java
@@ -3,8 +3,12 @@ package me.bokyujin.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import me.bokyujin.springbootdeveloper.domain.Article;
 import me.bokyujin.springbootdeveloper.dto.AddArticleRequest;
+import me.bokyujin.springbootdeveloper.dto.UpdateArticleRequest;
 import me.bokyujin.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,6 +17,28 @@ public class BlogService {
     private final BlogRepository blogRepository;
 
     public Article save(AddArticleRequest request){
+
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: "+id));
+    }
+
+    public void delete(long id){
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: "+id));
+        article.update(request.getTitle(),request.getContent());
+        return article;
     }
 }
